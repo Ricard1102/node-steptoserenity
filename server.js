bda01a520ad27e76b9aae58b874c0ed55b5998a1
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-05-08 14:30:27 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,142 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const hbs = require('hbs');
+const nodemailer = require('nodemailer');
+require('dotenv/config');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+//VIEW ENGINE SETUP
+hbs.registerPartials(__dirname + '/views/partials');
+//app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
+
+//STATIC FOLDER
+
+app.use('/public', express.static(path.join(__dirname + '/public')));
+
+//BODY PARSER MIDDLEWARE
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs',
+    {
+      //Head.hbs variables
+      keywords: 'reflexology',
+      metaDescription: '',
+      pageTitle: 'Step to Serenity',
+
+      //Header.hbs variables
+      logo: 'public/img/logo.png',
+      logoCaption: 'Step to Serenity logo',
+      logoTitle: 'Step to Serenity',
+      // menu1: '',
+      // menu2: '',
+      // menu3: '',
+      // menu4: '',
+      // menu5: '',
+      //Hero variables
+      h1Title: 'Step to Serenity',
+
+      webmail: 'info@steptoserenity.com',
+      twitter_url: '',
+      facebook_url: '',
+      googleplus_url: '',
+      instagram_url: '',
+      linkedin_url: '',
+
+      //Maps
+      address: 'Croydon',
+      map_link: 'https://www.google.com/maps/place/Croydon/@51.3678229,-0.1172588,13z/data=!3m1!4b1!4m5!3m4!1s0x4875fe2a84421673:0xd574f25dc50c4945!8m2!3d51.376165!4d-0.098234',
+
+      //Services variables
+      cancellationPolicy1: 'Step to Serenity operates a cancellation policy. Our policy is similar to many other medical clinics and we ask all patients kindly to adhere to it.',
+      cancellationPolicy2: 'Should you wish to cancel or reschedule an appointment we simply ask you to give a minimum of 24 hours notice. If this minimum is not adhered to, we reserve the right to charge 50% treatment cost of the appointment.',
+      cancellationPolicy3: 'We are aware that from time to time individual circumstances dictate that an appointment will be missed or less than the 24 hours notice will be given. On such occasions we can be lenient but frequent missed appointments can be very disruptive to the smooth running of Step to Serenity and can also be inconvenient to other patients that require an appointment slot',
+
+
+      //Contact Variables
+      businessPhone: '07533 777531'
+
+
+
+
+    });
+
+
+});
+
+app.post('/send', (req, res) => {
+  const output = `<p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone: ${req.body.phone}</li>
+    <li>Subject: ${req.body.subject}</li>
+    <li>Message: ${req.body.message}</li>
+    </ul>`;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.google.com',
+    port: 25, //587
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER, // generated ethereal user
+      pass: process.env.MAIL_PASS // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: process.env.MAIL_FROM, // sender address
+    to: process.env.MAIL_TO, // list of receivers
+    subject: 'Contact request', // Subject line
+    text: 'Hello world', // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+
+
+  transporter.sendMail(mailOptions, (error, info) => {
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.render('partials/thanks', { businessName: 'The Beute Clinic' });
+
+
+
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
+
+
+
+
+
+
+
