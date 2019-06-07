2030a47fb9cc0558fcf1644953654f2e53f51851
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2019-06-07 16:13:16 +0100

    dev

--- server.1.js
+++ server.1.js
@@ -89,25 +89,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-
-  if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-    return res.json({ "responseCode": 1, "responseDesc": "Please select captcha" });
-  }
-  // Put your secret key here.
-  var secretKey = "6LcdWHYUAAAAAFkewfd17GHdL7oo-KMmF09WKhsG";
-  // req.connection.remoteAddress will provide IP address of connected user.
-  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-  // Hitting GET request to the URL, Google will respond with success or error scenario.
-  request(verificationUrl, function (error, response, body) {
-    body = JSON.parse(body);
-    // Success will be true or false depending upon captcha validation.
-    if (body.success !== undefined && !body.success) {
-      return res.json({ "responseCode": 1, "responseDesc": "Failed captcha verification" });
-    }
-    res.json({ "responseCode": 0, "responseDesc": "Sucess" });
-  });
-
-
   const output = `<p>You have a new contact request</p>
   <h3>Contact Details</h3>
   <ul>
